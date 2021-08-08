685ce3566d84d3f4142abfb647a02270c84c6374
AnGgIt86 <jarbull86@gmail.com>
2021-08-08 22:37:03 +0700

    switch branch gcc server to trunk

--- build-gcc.sh
+++ build-gcc.sh
@@ -34,7 +34,7 @@ download_resources() {
   git clone git://sourceware.org/git/binutils-gdb.git -b master binutils --depth=1
   echo "Cloned binutils!"
   echo "Cloning GCC"
-  git clone git://gcc.gnu.org/git/gcc.git -b master gcc --depth=1
+  git clone git://gcc.gnu.org/git/gcc.git -b trunk gcc --depth=1
   cd ${WORK_DIR}
   echo "Downloaded prerequisites!"
 }
